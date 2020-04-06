01489ba7670326f3439a6a81272c2452f37326b4
Laura Gómez Cabero <lauragcabero@gmail.com>
2020-04-06 23:47:32 +0200

    tercer ejercicio js

---
+++ index.js
@@ -0,0 +1,17 @@
+var miArray = [['a', 'b'], ['c', 'd'], ['e', 'f']];
+
+// Indica la longitud del array:
+
+var longitud = miArray.length;
+
+// test
+
+console.log(longitud);
+
+// Indica la longitud de uno de los elementos del array y test:
+
+console.log(miArray[1].length);
+
+// Muestra por pantalla el valor del tercer elemento de la lista del array. Y dentro de ese tercer elemento, su segundo elemento:
+
+document.write(miArray[2][1]);
